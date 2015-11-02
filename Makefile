c7c0067c3eb2c5528f2b6d9fe5750b78adc525f7
Joe136 <JoeInTheSecond@gmx.de>
2015-11-02 13:06:10 +0100

    Lock memory only for system applications (uid < 1000). Append library to LD_PRELOAD. Modified Makefile.

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
+prefix = /usr/local
+
 all: libmlockall.so.0.0
 
 libmlockall.so.0.0: Makefile libmlockall.c
-	${CC} -fPIC -shared -Wl,-soname,libmlockall.so.0 -o libmlockall.so.0.0 -lc libmlockall.c  # -Wl,-export-dynamic
-	ln -fs libmlockall.so.0.0 libmlockall.so
-	ln -fs libmlockall.so.0.0 libmlockall.so.0
+	${CC} -fPIC -Os -shared -Wall -Wextra -Wpedantic -Wl,-soname,libmlockall.so.0 -o libmlockall.so.0.0 -lc libmlockall.c  # -Wl,-export-dynamic
+
 clean:
 	rm -Rf *.o lib*.so*
 
 # for checkinstall
 install:
-	install lib*.so* /usr/lib/
-	install mlockall /usr/bin/
+	install -s --mode=640 --group=mlockall libmlockall.so.0.0 "${prefix}/lib/"
+	ln -fs libmlockall.so.0.0 "${prefix}/lib/libmlockall.so"
+	ln -fs libmlockall.so.0.0 "${prefix}/lib/libmlockall.so.0"
--- libmlockall.c
+++ libmlockall.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/mman.h>
 
+
+
+#ifndef MAX_UID
+#define MAX_UID 999
+#endif
+#ifndef LIB_NAME
+#define LIB_NAME "libmlockall.so"
+#endif
+#ifndef LIB_VER
+#define LIB_VER ".0.0"
+#endif
+
+
+
 void __attribute__((constructor)) init() {
-    if(mlockall(MCL_CURRENT|MCL_FUTURE)) {
-	perror("mlockall");
-    }
+   if (getuid () <= MAX_UID) {
+      if (mlockall (MCL_CURRENT|MCL_FUTURE) ) {
+         perror ("mlockall");
+      } else {
+         //printf ("memory locked\n");
+
+         // Append library to LD_PRELOAD
+         char   *library    = LIB_NAME LIB_VER ":";
+         size_t  llen       = strnlen (library, 255);
+         size_t  olen       = 0;
+         char   *preload    = NULL;
+         char   *oldPreload = getenv ("LD_PRELOAD");
+
+
+         if (oldPreload) {   // getenv returns a NULL pointer and strnlen/strncpy cannot handle it
+            olen = strnlen (oldPreload, 10000);
+
+            if (strstr (oldPreload, LIB_NAME) ) return;   // Check if library is allready a part of LD_PRELOAD
+         }
+
+         preload = (char*)malloc (olen + llen + 1);   //TODO check memory
+
+         strncpy (preload, library, llen);
+
+         if (oldPreload) {
+            strncpy (&(preload[llen]), oldPreload, olen);
+         }
+
+         preload[llen + olen] = 0;
+
+         printf ("preload: %s\n", preload);
+
+         setenv ("LD_PRELOAD", preload, 1);
+
+         free (preload);
+      }
+   } //else { printf ("getuid above %i\n", MAX_UID); }
 }
